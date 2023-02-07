54247dc7b7d19516ffe8322635c18ba98e1ab762
Bertwald <thomas_ace8@hotmail.com>
2023-02-07 13:34:39 +0100

    New stuff

--- MoldAndBold/Models/DataLoader.cs
+++ MoldAndBold/Models/DataLoader.cs
@@ -28,37 +28,59 @@ namespace MoldAndBold.Models
 
         internal static void LoadData()
         {
+            // StreamReader can fail
+            try {
+                using (StreamWriter writer = new ("../../../" + "LocalOnly/AggregatedMonths.txt"))
+                using (StreamReader reader = new ("../../../" + "LocalOnly/tempdata5-med fel.txt")) {
+                    //string regexString11 = """(?<date>\d{4}-\d{2}-\d{2}) (?<time>\d{2}:\d{2}:\d{2}),(?<location>Inne|Ute),(?<temp>-?\d{1,2}.\d{1,2}),(?<moisture>\d{1,2})""";
+                    string regexString = "(?<date>\\d{4}-\\d{2}-\\d{2}) (?<time>\\d{2}:\\d{2}:\\d{2}),(?<location>Inne|Ute),(?<temp>-?\\d{1,2}.\\d{1,2}),(?<moisture>\\d{1,2})";
+                    Regex regex = new("(?<date>\\d{4}-\\d{2}-\\d{2}) (?<time>\\d{2}:\\d{2}:\\d{2}),(?<location>Inne|Ute),(?<temp>-?\\d{1,2}.\\d{1,2}),(?<moisture>\\d{1,2})");
 
-            using (StreamReader reader = new StreamReader("../../../" + "LocalOnly/tempdata5-med fel.txt"))
-            {
-                string regexString = "(?<date>\\d{4}-\\d{2}-\\d{2}) (?<time>\\d{2}:\\d{2}:\\d{2}),(?<location>Inne|Ute),(?<temp>-?\\d{1,2}.\\d{1,2}),(?<moisture>\\d{1,2})";
-                Regex regex = new Regex("(?<date>\\d{4}-\\d{2}-\\d{2}) (?<time>\\d{2}:\\d{2}:\\d{2}),(?<location>Inne|Ute),(?<temp>-?\\d{1,2}.\\d{1,2}),(?<moisture>\\d{1,2})");
 
-
-                string rows = reader.ReadToEnd();
-                var test = rows.Split('\n').GroupBy(x => x[..10]).ToList();
-                var matches = regex.Matches(rows).GroupBy(x => x.Groups["date"]).Select(x => x.First()).ToList();
-
-                foreach (Match match in Regex.Matches(rows, regexString))
-                {
-                    if (ValidateDateInput(match))
-                    {
-                        foreach (Group group in match.Groups)
-                        {
-                            Console.WriteLine(group.Name + " " + group.Value);
-                        }
-
-                        Console.WriteLine(match.Name);
+                    string rows = reader.ReadToEnd();
+                    var test = rows.Split(Environment.NewLine).GroupBy(x => x[..10]).ToList();
+                    foreach(var t in test) {
+                        Console.WriteLine($"{t.Key}: Has values");
+                        // Loop and write values
+                        // foreach()
                     }
-                }
 
+                    //var matches = regex.Matches(rows).GroupBy(x => x.Groups["date"]).Select(x => x.First()).ToList();
+
+                    //foreach (Match match in Regex.Matches(rows, regexString))
+                    //{
+                    //    if (ValidateDateInput(match))
+                    //    {
+                    //        foreach (Group group in match.Groups)
+                    //        {
+                    //            Console.WriteLine(group.Name + " " + group.Value);
+                    //        }
+
+                    //        Console.WriteLine(match.Name);
+                    //    }
+                    //}
+
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("A File could not be used:");
+                Console.WriteLine(e.Message);
             }
         }
 
-        private static bool ValidateDateInput(Match match)
+        private static bool ValidateDate(Match match)
         {
             return DateTime.TryParse(match.Groups["date"].ToString(), out _);
 
+        }
+        private static bool ValidateTime(Match match) {
+            return true;
+        }
+        private static bool ValidateTemperature(Match match) {
+            return true;
+        }
+        private static bool ValidateMoisture(Match match) {
+            return true;
         }
 
         internal void GenerateAggregatedData()
