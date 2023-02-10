d722a19f8fd7fc0ffabe1cf90ee28c14e1c64499
Bertwald <thomas_ace8@hotmail.com>
2023-02-10 15:58:55 +0100

    Menumenthods

---
+++ MoldAndBold/GUI/MenuMethods.cs
@@ -0,0 +1,31 @@
+﻿using MoldAndBold.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoldAndBold.GUI
+{
+    internal static class MenuMethods
+    {
+        internal static void ShowInsideData()
+        {
+            ActionSelector.ExecuteActionFromList(new List<Action> { SearchByDate, ShowDaysOrderedByTemp, ShowDaysOrderedByHumidity, ShowDaysOrderedByMoldRisk });
+        }
+        internal static void ShowOutsideData() 
+        {
+            ActionSelector.ExecuteActionFromList(new List<Action> { SearchByDate, ShowDaysOrderedByTemp, ShowDaysOrderedByHumidity, ShowDaysOrderedByMoldRisk, ShowSpecialDates });
+        }
+        internal static void ShowDaysOrderedByTemp()
+        { }
+        internal static void ShowDaysOrderedByHumidity() 
+        { }
+        internal static void ShowDaysOrderedByMoldRisk()
+        { }
+        internal static void ShowSpecialDates()
+        { }
+        internal static void SearchByDate()
+        { }
+    }
+}
